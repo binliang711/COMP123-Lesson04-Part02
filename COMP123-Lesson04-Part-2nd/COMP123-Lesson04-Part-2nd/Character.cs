da33e5b397cad7aadca0fd38166389369fe056a1
Bin Liang <bingliang711@hotmail.com>
2015-01-28 20:07:39 -0700

    Improve Character class

---
+++ COMP123-Lesson04-Part-2nd/COMP123-Lesson04-Part-2nd/Character.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_Lesson04_Part_2nd
+{
+    //CHARACTER SUPER CLASS +++++++++++++++++++++++++++++++++++++++++++++++++++
+    class Character
+    {
+        //PRIVATE PROPERTIES ++++++++++++++++++++++++++++++++++++++++++++++++++
+        protected string name; 
+        protected int health;
+        protected int smarts;
+        protected string gender;
+        //CONSTRUCTOR +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        public Character(string name, int health, int smarts, string gender)
+        {
+            this.name = name;
+            this.health = health;
+            this.smarts = smarts;
+            this.gender = gender;//male or female
+        }
+
+        // PROTECTED METHODS +++++++++++++++++++++++++++++++++++++++++++++++++++++
+        protected void showProfile()
+        {
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("Name:{0}", this.name);
+            Console.WriteLine("Health:{0}", this.health);
+            Console.WriteLine("Smarts:{0}", this.smarts);
+            Console.WriteLine("Gender:{0}", this.gender);
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++");
+        }
+    }
+}
